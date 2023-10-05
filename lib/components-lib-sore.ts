bd70e8fd9c829242cfa5d1722464bad40204358f
Vladimir <trofimyuk@gmail.com>
2023-10-05 17:52:02 +0300

    Wow

---
+++ lib/components-lib-sore.ts
@@ -0,0 +1,5 @@
+//components-lib-sore.ts
+import { Button } from "./components/Button";
+import { Header } from "./components/Header";
+
+export { Button, Header };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
+import dts from "vite-plugin-dts"
+import { libInjectCss } from "vite-plugin-lib-inject-css"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: "lib" }), libInjectCss()],
+  build: {
+    // do not copy the contents of the public folder to the dist folder
+    copyPublicDir: false,
+    lib: {
+      // this is the file that exports our components
+      entry: resolve(__dirname, "lib/components-lib-sore.ts"),
+      name: "ViteStorybookLibrary",
+      fileName: "components-lib-sore",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 })
